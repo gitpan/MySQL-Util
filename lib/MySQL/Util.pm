81f9a873d1c57bbea64f58d4ddbb58d28b88bdb6
John Gravatt <gravattj@cpan.org>
2011-11-02 19:02:53 +0000

    Import of GRAVATTJ/MySQL-Util-0.02 from CPAN.
    gitpan-cpan-distribution: MySQL-Util
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GRAVATTJ/MySQL-Util-0.02.tar.gz
    gitpan-cpan-author:       GRAVATTJ
    gitpan-cpan-maturity:     released

--- lib/MySQL/Util.pm
+++ lib/MySQL/Util.pm
@@ -38,11 +38,11 @@ MySQL::Util - Utility functions for working with MySQL.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
